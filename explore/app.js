522d755e1ae343e5f7ec28f7c13d6e6fab7c389c
Trenton Wuerker <trenton.wuerker@gmail.com>
2017-08-05 11:11:13 -0600

    added auto-centered map

--- explore/app.js
+++ explore/app.js
@@ -1,11 +1,23 @@
-$(document).ready(function () {
-  myMap()
-})
+(function() {
+  var map;
 
-function myMap() {
-var mapProp= {
-    center:new google.maps.LatLng(39.7392,-104.9903),
-    zoom:10,
-};
-var map=new google.maps.Map(document.getElementById("googleMap"),mapProp);
-}
+  var mapOptions = {
+    zoom: 10,
+    mapTypeId: google.maps.MapTypeId.ROADMAP,
+    center: new google.maps.LatLng(39.7392, -104.9903)
+  };
+  map = new google.maps.Map(document.getElementById('googleMap'), mapOptions);
+
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function(position) {
+      var geolocate = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+      map.setCenter(geolocate);
+      map.setZoom(15);
+
+      var marker = new google.maps.Marker({
+        position: geolocate
+      });
+      marker.setMap(map);
+    });
+  }
+})();
