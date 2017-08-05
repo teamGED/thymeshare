2d50777662b9d9f4ec3dc1ed3ea79ff95a853c52
Trenton Wuerker <trenton.wuerker@gmail.com>
2017-08-05 12:13:00 -0600

    added gradient to all pages

--- explore/app.js
+++ explore/app.js
@@ -1,23 +1,49 @@
-(function() {
-  var map;
+const url =
 
-  var mapOptions = {
-    zoom: 10,
-    mapTypeId: google.maps.MapTypeId.ROADMAP,
-    center: new google.maps.LatLng(39.7392, -104.9903)
-  };
-  map = new google.maps.Map(document.getElementById('googleMap'), mapOptions);
+  (function() {
+    var map;
 
-  if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(function(position) {
-      var geolocate = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
-      map.setCenter(geolocate);
-      map.setZoom(15);
+    var mapOptions = {
+      zoom: 5,
+      mapTypeId: google.maps.MapTypeId.ROADMAP,
+      center: new google.maps.LatLng(39.7392, -104.9903)
+    };
+    map = new google.maps.Map(document.getElementById('googleMap'), mapOptions);
 
-      var marker = new google.maps.Marker({
-        position: geolocate
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(function(position) {
+        var geolocate = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+        map.setCenter(geolocate);
+        map.setZoom(12);
       });
-      marker.setMap(map);
-    });
-  }
-})();
+    }
+
+    function getAddress() {
+
+      var addressInput = '3611 osage st';
+
+      var geocoder = new google.maps.Geocoder();
+
+      geocoder.geocode({
+        address: addressInput
+      }, function(results, status) {
+
+        if (status == google.maps.GeocoderStatus.OK) {
+
+          var myResult = results[0].geometry.location; // reference LatLng value
+
+          createMarker(myResult); // call the function that adds the marker
+
+        }
+      });
+    }
+
+    function createMarker(latlng) {
+
+      marker = new google.maps.Marker({
+        map: map,
+        position: latlng
+      });
+
+    }
+  })();
