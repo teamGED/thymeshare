b95e65fc4c4caab3cd4beddf9f4adcafd9dd079c
EllenDown <ellenldowning@gmail.com>
2017-08-09 10:49:52 -0600

    commit changes

--- signUp/app.js
+++ signUp/app.js
@@ -24,9 +24,6 @@ $('#sellerSignUp').click(function(event){
       address,
       item
     };
-
-    // $.post('http://localhost:8080/api/v1/persons/seller/signup', newSellerPost)
-
     $.post('https://warm-tor-27276.herokuapp.com/api/v1/persons/seller/signup', newSellerPost)
     .then(response => {
       clearInput();
