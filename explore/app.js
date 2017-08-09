690bc1a08a9d2cbd5e7787494b8407ad09b89d8f
EllenDown <ellenldowning@gmail.com>
2017-08-09 09:56:48 -0600

    commit css changes and add login

--- explore/app.js
+++ explore/app.js
@@ -105,3 +105,27 @@ $.get(url+ "/names").then((names) => {
     })
  })
 })
+
+$('#signup').click(function(event) {
+  event.preventDefault();
+  location.href = "signUp/index.html";
+});
+
+$('#validateLogin').click(function(event) {
+  event.preventDefault()
+  const email = $('#email').val()
+  const password = $('#password').val()
+  const data = {
+    email,
+    password
+  }
+  $.post("http://localhost:8080/api/v1/persons/login", data)
+    .then(response => {
+      if (response.error) {
+        alert(response.error)
+      } else {
+        localStorage.setItem('token', response.data)
+        location.href = '../seller/index.html'
+      }
+    })
+})
